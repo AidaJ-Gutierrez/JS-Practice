7b09078ccf117f1b2f05a0d0b56ef9912d5cb543
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-04-05 14:12:22 -0500

    Start to-do list js template, ejs package, nodemon package, node and express

---
+++ todolist-v1/app.js
@@ -0,0 +1,30 @@
+
+// template,
+
+const express = require("express");
+const bodyParser = require ("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.get("/", function (req, res){
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    if (currentDay === 6 || currentDay === 0){
+        day = "weekend";
+    }else{
+        day ="weekday";
+    }
+    res.render('list', {kindOfDay: day});
+});
+
+app.listen(3000, function (){
+    console.log("Server started on por 3000")
+});
+
+
+
