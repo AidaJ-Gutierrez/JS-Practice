039e529cfeb132dfbf39dce827b3623db255ba7a
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-04-06 14:37:59 -0500

    add node module date with .exports

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,6 +4,10 @@
 const express = require("express");
 const bodyParser = require ("body-parser");
 
+const  date = require(__dirname + "/date.js")
+
+
+
 const app = express();
 
 
@@ -17,16 +21,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+
+
+
 app.get("/", function (req, res){
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    let day= date();
 
     res.render('list', {listTitle: day, newListItems: items});
 });
