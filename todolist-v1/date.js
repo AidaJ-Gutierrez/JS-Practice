7497a4ba8f22b09400c79eb519e994873d4ea0ac
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-04-06 14:49:20 -0500

    finish ejs and templates

--- todolist-v1/date.js
+++ todolist-v1/date.js
@@ -2,18 +2,28 @@
 //jshint esversion:6
 
 
-module.exports = getDate;
+exports.getDate = function (){
+   const today = new Date();
 
-
-function getDate(){
-    let today = new Date();
-
-    let options = {
+    const options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    let day = today.toLocaleDateString("en-US", options);
-    return day;
-}
+    return today.toLocaleDateString("en-US", options);
+
+};
+
+
+
+exports.getDay = function (){
+    const today = new Date();
+
+    const options = {
+        weekday: "long",
+    };
+
+    return today.toLocaleDateString("en-US", options);
+
+};
