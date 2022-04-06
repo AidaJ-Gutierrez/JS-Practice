bc350fb5006347b38131abb19852b294b59e0636
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-04-06 14:18:52 -0500

    add header and footer partials

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -7,7 +7,10 @@ const bodyParser = require ("body-parser");
 const app = express();
 
 
-var items= [];
+//get and post methods for list and work pages
+
+let items= [];
+let workItems =[];
 
 
 app.set('view engine', 'ejs');
@@ -15,32 +18,56 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function (req, res){
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
-    res.render('list', {kindOfDay: day, newListItems: items});
+    res.render('list', {listTitle: day, newListItems: items});
 });
 
 
 app.post("/", function (req, res){
-   var item =req.body.newItem;
+    let item =req.body.newItem;
 
-   items.push(item);
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
 
-    res.redirect("/");
 });
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+
+
+
+// post and get methods for about page
+
+app.get("/about", function (req,res){
+   res.render("about");
+});
+
+
+
+
+// app listener
 
 app.listen(3000, function (){
     console.log("Server started on por 3000")
 });
+
+
 
 
 
