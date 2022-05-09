7db997d1089ac94eafbd4555fda327b9e644bedb
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2022-05-09 10:39:21 -0500

    added post view, js code for view and lodash utility

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const {value} = require("lodash/seq");
+const _ = require("lodash");
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,22 +22,30 @@ app.use(express.static("public"));
 
 var posts = [];
 
+// home page
 app.get("/", function(req, res){
   res.render("home", {
     startingContent: homeStartingContent,
     posts: posts
   });
 });
+
+// about page
 app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent})
 });
+
+//contact page
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent})
 });
 
+//compose page
 app.get("/compose", function(req, res){
   res.render("compose" );
 });
+
+// redirect to home  after post has been created
 app.post("/compose", function(req, res){
   const post = {
     title: req.body.postTitle,
@@ -42,6 +53,22 @@ app.post("/compose", function(req, res){
   };
   posts.push(post);
   res.redirect("/");
+});
+
+// find a post by title utilizing lodash
+app.get('/posts/:postName', (req, res) => {
+ const requestedTitle =_.lowerCase(req.params.postName);
+  posts.forEach(function(post){
+   const storedTitle = _.lowerCase(post.title);
+
+   if (storedTitle === requestedTitle){
+
+     res.render("post",{
+         title: post.title,
+         content: post.content
+       });
+   }
+  });
 });
 
 
